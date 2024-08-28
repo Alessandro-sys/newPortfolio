76f2907db02c1cd0d9f32fc90436070d77b8ab15
Alessandro <71657415+Alessandro-sys@users.noreply.github.com>
2024-08-28 21:17:16 +0200

    home flask

---
+++ app.py
@@ -0,0 +1,30 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+import base64
+from PIL import Image
+
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/home")
+def pages():
+    return redirect("/")
