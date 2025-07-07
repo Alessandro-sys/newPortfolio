6935ce8b5287cb0ce3a2517f1b30db030fb4930c
Alessandro <chiarulli14@gmail.com>
2025-07-07 11:58:59 +0200

    modifica home screen
    cambiato il sottotitolo e rimossa la card del sito di coppia

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def robocupIt():
 def insta():
     db = get_db()
     cursor1 = db.cursor()
-    
+
     # Update the data table
     cursor1.execute("""
         UPDATE data
@@ -85,68 +85,7 @@ def insta():
 
     return redirect("/")
 
-
-@app.route("/stats")
-def stats():
-    db = get_db()
-    cursor3 = db.cursor()
-
-    cursor3.execute("SELECT * FROM data")
-    accessi = cursor3.fetchall()
-
-    cursor3.execute("SELECT * FROM access_log")
-    log = cursor3.fetchall()
-
-    db.commit()
-
-    return render_template("table.html", accessi = accessi, logs = log)
-
 @app.route("/docmost")
 def docmost():
-    return render_template()
-    
-
-
-
-# def scheduled_task():
-#     with app.app_context():  # Crea un contesto applicativo per la durata della funzione
-#         db = get_db()
-#         cursor = db.cursor()
-
-#         current_date = datetime.now()
-#         yesterday_date = (current_date - timedelta(days=1)).strftime('%Y-%m-%d')
-
-#         cursor.execute("SELECT * FROM access_log WHERE date = ?", (yesterday_date))
-#         logs = cursor.fetchall()
-
-#         db.commit()
-
-#         cursor.execute("SELECT * FROM data WHERE platform = 'instagram'")
-#         n_accessi = cursor.fetchall()
-
-#         db.commit()
-
-#         n_accessi = n_accessi[0][1]
-
-#         yesterday_date_formatted = yesterday_date.strftime('%d-%m-%Y')
-       
-
-#         emailBody = Template('''
-# Daily log 
-
-# Access log in data $data: $tot
-
-# $log.
-
-# ''')
-#         formatted_logs = format_logs(logs)
-#         body = emailBody.substitute(data = yesterday_date_formatted, tot = n_accessi, log = formatted_logs)
-#         subject = "Daily Log"
-#         sendEmail(body, me, subject)
-
-# # Configura APScheduler per eseguire `scheduled_task` ogni giorno a mezzanotte
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=scheduled_task, trigger=CronTrigger(hour=8, minute=0))
-# scheduler.start()
-
+    return "benvenuto su docmost"
 
